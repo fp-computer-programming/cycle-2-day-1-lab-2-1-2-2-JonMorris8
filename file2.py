31960d174f55f18ba780c57ac9c8414b8b2e17fd
p24jmorris <p24jmorris@fairfieldprep.org>
2023-09-19 13:38:30 -0400

    Lab 2-1 and 2-2. I named the files in visual but the name does not show up here.

--- file2.py
+++ file2.py
@@ -1,13 +1,15 @@
 # 1) Create a Python file named lab_2-2.py
 
 # 2) Set a equal to 3 and b equal to a
-
+a = 3
+b = a
 # 3) Print the value of b and the value of a.
-
+print ( b,a )
 # 4) What is the value of each variable and why? (Answer as a comment)
-
+#it came up with 2 3's spaced apart
 # 5) Set a equal to 7.
-
+a = 7
 # 6) Print the value of a and the value of b
-
+print ( a,b )
 # 7) What is the value of each variable and why? (Answer as a comment)
+#a switched to 7 as b kept same number as it was ran before
